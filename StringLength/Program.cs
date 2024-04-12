9287b75fc7259dbdfa2956693ce4d170be69e6af
jay sharma <jay sharma@LAPTOP-90F1PJNM>
2024-04-12 14:13:20 +0530

    "added new program"

---
+++ StringLength/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Enter the string :");
+            input = Console.ReadLine();
+            int x =0;
+            foreach (char c in input)
+            {
+                Console.WriteLine(input[x]);
+                x++;
+            }
+            Console.WriteLine("\nLength of string:{0}", (x));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Substrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substrings
+{
+    public class Program
+    {
+        public static List<string> GetAllSubstrings(string str)
+        {
+            List<string> substrings = new List<string>();
+            for (int i = 0; i < str.Length; i++) 
+            {
+                for (int j = i +1; j <= str.Length; j++)
+                {
+                    string substring = str.Substring(i, j - i);
+                    substrings.Add(substring);
+
+                }
+            }
+            return substrings;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Enter the string :");
+            string input = Console.ReadLine();
+            List<string> substrings = GetAllSubstrings(input);
+            Console.WriteLine("substring of \"" + input + "\":");
+            Console.ReadLine();
+            foreach (string substring in substrings)
+            {
+                Console.WriteLine(substring);
+                Console.ReadLine();
+            }
+        }
+    }
+}
